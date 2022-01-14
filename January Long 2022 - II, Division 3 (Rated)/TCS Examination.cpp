bf2b439de74a42b72161e671b6f26c99432e77ea
Abbas Shaikh <61411096+Abbas12321@users.noreply.github.com>
2022-01-14 16:06:32 +0530

    Create TCS Examination.cpp

---
+++ January Long 2022 - II, Division 3 (Rated)/TCS Examination.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int TESTCASE=1;
+    cin>>TESTCASE; 
+    while (TESTCASE --)
+    {
+        int a,b,c,d,e,f; 
+        cin>>a>>b>>c>>d>>e>>f; 
+        if(a+b+c>d+e+f)
+        {
+            cout << "DRAGON\n";
+        }
+        else if(a+b+c<d+e+f)
+        {
+            cout << "SLOTH\n";
+        }
+        else
+        {
+            if(a>d) cout << "DRAGON\n"; 
+            else if (d>a) cout << "SLOTH\n"; 
+            else
+            {
+                if(b>e) cout << "DRAGON\n";
+                else if(e>b) cout << "SLOTH\n"; 
+                else cout << "TIE\n";
+            }
+
+        }
+    }
+}
